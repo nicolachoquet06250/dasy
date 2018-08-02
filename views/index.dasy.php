81c7966c820fdded8828a15aaa618954573139a6
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-08-02 11:05:49 +0200

    integration d'une nouvelle syntaxe type javascript

--- views/index.dasy.php
+++ views/index.dasy.php
@@ -16,3 +16,27 @@
 		$i++;
 	});
 });
+
+<php>
+	let i=0;
+
+	let max=10;
+
+	const TEST='lol';
+
+	define TEST2='test constante';
+
+	test->each( value => {
+		print value + '<br>\n\n';
+	});
+
+	for(i=0, max=10; i < max; i++) {
+		print i + '<br>\n';
+	}
+
+	while(test > 2) {
+		if(i>2) {
+			print i+'<br>\n';
+		}
+	}
+</php>
