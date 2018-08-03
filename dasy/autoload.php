eb3a598d6954c69ae7fbb509a29cd3c0e25741fb
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-08-03 16:31:30 +0200

    restructure pour avoir le moins de fichiers seuls possible.

--- dasy/autoload.php
+++ dasy/autoload.php
@@ -1,25 +1,33 @@
 <?php
 
+// Interfaces
 require_once "interfaces/module.php";
 
-require_once "php_bloc.php";
-require_once "dasy_cache.php";
-
+// Functions
 require_once "functions/dump.php";
 
+// Conditions
 require_once "conditions/if_condition.php";
 require_once "conditions/else_condition.php";
 require_once "conditions/elseif_condition.php";
 require_once "conditions/switch_condition.php";
 
+// Loops
 require_once "loops/for_loop.php";
 require_once "loops/foreach_loop.php";
 require_once "loops/while_loop.php";
 
+// Variables
 require_once "variables/variable.php";
 require_once "variables/constante.php";
 
-require_once "file_parser.php";
-require_once "dasy_parser.php";
+// Files
+require_once "files/file_parser.php";
 
-require_once "Dasy.php";
+// Php
+require_once "php/php_bloc.php";
+
+// Dasy
+require_once "dasy/dasy_cache.php";
+require_once "dasy/dasy_parser.php";
+require_once "dasy/Dasy.php";
